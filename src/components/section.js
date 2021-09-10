88b7360536e5dddad163035cc585a88aaccb3d8a
JhoanBurbano <84604960+JhoanBurbano@users.noreply.github.com>
2021-09-09 20:13:28 -0500

    X

--- src/components/section.js
+++ src/components/section.js
@@ -14,6 +14,8 @@ function Section(){
                 <p className="describe">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste voluptatibus minus nobis laboriosam vel sint labore impedit odio facere quidem, repellat eaque tenetur assumenda iusto ducimus qui eos, eveniet vitae?</p>
                 </div>
             <div className="img">
+            <img src="https://concepto.de/wp-content/uploads/2018/04/base-de-datos-min-e1523470739502.jpg" alt="" />
+                <img src="https://concepto.de/wp-content/uploads/2018/04/base-de-datos-min-e1523470739502.jpg" alt="" />
                 <img src="https://concepto.de/wp-content/uploads/2018/04/base-de-datos-min-e1523470739502.jpg" alt="" />
             </div>
             </div>
